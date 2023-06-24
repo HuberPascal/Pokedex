13d416e64767a975a84d58c5e92267866dd9d4a7
Pascal Huber <pascalhuber@Pascals-MacBook-Pro.local>
2023-06-24 16:01:36 +0200

    überarbeitet

--- script.js
+++ script.js
@@ -1,127 +1,157 @@
 let pokemon = [
-    'bulbasaur', 'ivysaur', 'venusaur', 'squirtle', 'charmander', 'pikachu', 'jigglypuff', 'meowth', 'psyduck', 'machop', 'geodude', 'gyarados', 'lapras', 'eevee', 'snorlax', 'articuno', 'zapdos', 'moltres', 'moltres', 'chikorita', 'cyndaquil', 'totodile', 'togepi', 'mareep', 'typhlosion', 'feraligatr', 'lugia', 'ho-oh', 'treecko', 'torchic', 'mudkip', 'gardevoir', 'sceptile', 'blaziken', 'swampert', 'mightyena', 'zigzagoon', 'linoone', 'wurmple', 'silcoon', 'beautifly', 'cascoon', 'dustox', 'lotad', 'lombre', 'ludicolo', 'seedot', 'nuzleaf', 'shiftry', 'taillow', 'swellow', 'wingull', 'pelipper', 'ralts', 'kirlia', 'gardevoir', 'surskit', 'masquerain', 'shroomish', 'breloom', 'slakoth', 'vigoroth', 'slaking', 'nincada', 'ninjask', 'shedinja', 'whismur', 'loudred', 'exploud', 'makuhita', 'hariyama', 'azurill', 'nosepass', 'skitty', 'delcatty', 'sableye', 'mawile', 'aron', 'lairon', 'aggron', 'meditite', 'medicham', 'electrike', 'manectric', 'plusle', 'minun', 'volbeat', 'illumise', 'roselia', 'gulpin', 'swalot', 'carvanha', 'sharpedo', 'wailmer', 'wailord', 'numel', 'camerupt', 'torkoal',];
-  
-
+  'bulbasaur', 'ivysaur', 'venusaur', 'squirtle', 'charmander', 'pikachu', 'jigglypuff', 'meowth', 'psyduck', 'machop', 'geodude', 'gyarados', 'lapras', 'eevee', 'snorlax', 'articuno', 'zapdos', 'moltres', 'chikorita', 'cyndaquil', 'totodile', 'togepi'
+];
 
 let currentPokemon;
 
 async function loadPokemon() {
-    for (let i = 0; i < pokemon.length; i++) {
-        const pokemonNames = pokemon[i];
-        
-    let url = `https://pokeapi.co/api/v2/pokemon/${pokemonNames}`;
-    let response = await fetch(url);
-    currentPokemon = await response.json();  
-    console.log('Loaded pokemon', currentPokemon);
-    loadPokemonCards(i);
-}
-    
-    
+  for (let i = 0; i < pokemon.length; i++) {
+      const pokemonName = pokemon[i];
+
+      let url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
+      let response = await fetch(url);
+      currentPokemon = await response.json();
+      console.log('Geladenes Pokémon:', currentPokemon);
+      loadPokemonCards(i, currentPokemon);
+  }
 }
 
+function renderPokemonInfo(index) {
+  document.getElementById('pokedex').classList.remove('dNone');
+  document.getElementById('pokedexInfoArea').classList.remove('dNone');
 
+  let pokemonNameContainer = document.getElementById('pokemonName');
+  let pokemonName = pokemon[index];
+  pokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+  pokemonNameContainer.innerHTML = pokemonName;
 
-function renderPokemonInfo(i) {
-    document.getElementById('pokedex').classList.remove('dNone');
-    document.getElementById('pokedexInfoArea').classList.remove('dNone');
+  let pokemonImage = document.getElementById('pokemonImg');
+  pokemonImage.src = currentPokemon['sprites']['other']['official-artwork']['front_default'];
 
-
-    let = pokekomNameContainer = document.getElementById('pokemonName');
-    let pokemonName = currentPokemon['name'];
-    pokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
-    pokekomNameContainer.innerHTML = pokemonName;
-    document.getElementById('pokemonImg').src = currentPokemon['sprites']['other']['home']['front_default'];
-    renderAbilities();
+  renderAbilities(currentPokemon);
 }
 
-function renderAbilities() {
-    let stats = currentPokemon['stats'];
-    let abilities = document.getElementById('abilities');
-    
+function renderAbilities(currentPokemon) {
+  let stats = currentPokemon['stats'];
+  let abilities = document.getElementById('abilities');
 
-    for (let i = 0; i < stats.length; i++) {
-        let statsName = currentPokemon['stats'][i]['stat']['name'];
-        let baseStat = currentPokemon['stats'][i]['base_stat'];
+  abilities.innerHTML = ''; // Vorherige Fähigkeiten löschen
 
-        if(statsName === 'hp') {
-            statsName = 'HP';
-        }
+  for (let i = 0; i < stats.length; i++) {
+      let statsName = stats[i]['stat']['name'];
+      let baseStat = stats[i]['base_stat'];
 
-        if(statsName === 'special-attack') {
-            statsName = 'Sp. Attack';
-        }
+      if (statsName === 'hp') {
+          statsName = 'KP';
+      } else if (statsName === 'special-attack') {
+          statsName = 'Spez. Angriff';
+      } else if (statsName === 'special-defense') {
+          statsName = 'Spez. Verteidigung';
+      }
 
-        if(statsName === 'special-defense') {
-            statsName = 'Sp. Defense';
-        }
+      statsName = statsName.charAt(0).toUpperCase() + statsName.slice(1);
 
-        statsName = statsName.charAt(0).toUpperCase() + statsName.slice(1);
+      let progressBarClass = baseStat > 50 ? 'bg-success' : 'bg-danger';
 
-
-        abilities.innerHTML += /*html*/ `
-        <div class="flex">
-                    <p>${statsName}</p>
-                    <span>${baseStat}</span>
-                    <div class="progressInfo">
-                        <div
-                            class="progress"
-                            role="progressbar"
-                            aria-label="Success example"
-                            aria-valuenow="25"
-                            aria-valuemin="0"
-                            aria-valuemax="100">
-                            <div
-                                class="progress-bar bg-success"
-                                style="width: ${baseStat}%">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-        `;
-    }
+      abilities.innerHTML += `
+          <div class="flex">
+              <p>${statsName}</p>
+              <span>${baseStat}</span>
+              <div class="progressInfo">
+                  <div class="progress" role="progressbar" aria-label="Erfolgsbeispiel" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                      <div class="progress-bar ${progressBarClass}" style="width: ${baseStat}%"></div>
+                  </div>
+              </div>
+          </div>
+      `;
+  }
 }
 
 
 
 
 //////////// render Funktion Startseite
-function loadPokemonCards(i) {
-    let cardsArea = document.getElementById('cardsArea');
-    let pokemonName = currentPokemon['name'];
-    pokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
-    let pokemonImageSrc = currentPokemon['sprites']['other']['home']['front_default'];
-    let types = currentPokemon['types'][0]['type']['name'];
-    let typesSecond = "";
-  
-    if (currentPokemon['types'].length >= 2) {
+function loadPokemonCards(index, currentPokemon) {
+  let cardsArea = document.getElementById('cardsArea');
+  let pokemonName = pokemon[index];
+  pokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+  let pokemonImageSrc = currentPokemon['sprites']['other']['official-artwork']['front_default'];
+  let types = currentPokemon['types'][0]['type']['name'];
+  let typesSecond = "";
+
+  if (currentPokemon['types'].length >= 2) {
       typesSecond = currentPokemon['types'][1]['type']['name'];
       cardsArea.innerHTML += `
-        <div onclick="renderPokemonInfo(${i})" id="container${i}">
-          <div id="cards" class="${types}">
-            <h2>${pokemonName}</h2>
-            <div class="pokemonType">
-              <b>${types}</b>
-            </div>
-            <div class="pokemonTypeSecond">
-              <b>${typesSecond}</b>
-            </div>
-            <img id="pokemonNameCards" src="${pokemonImageSrc}">
+          <div onclick="renderPokemonInfo(${index})" id="container${index}">
+              <div id="cards" class="${types}">
+                  <h2>${pokemonName}</h2>
+                  <div class="pokemonType">
+                      <b>${types}</b>
+                  </div>
+                  <div class="pokemonTypeSecond">
+                      <b>${typesSecond}</b>
+                  </div>
+                  <img id="pokemonNameCards" src="${pokemonImageSrc}">
+              </div>
           </div>
-        </div>
       `;
-    } else {
+  } else {
       cardsArea.innerHTML += `
-        <div onclick="renderPokemonInfo(${i})" id="container${i}">
-          <div id="cards" class="${types}">
-            <h2>${pokemonName}</h2>
-            <div class="pokemonType">
-              <b>${types}</b>
-            </div>
-            <img id="pokemonNameCards" src="${pokemonImageSrc}">
+          <div onclick="renderPokemonInfo(${index})" id="container${index}">
+              <div id="cards" class="${types}">
+                  <h2>${pokemonName}</h2>
+                  <div class="pokemonType">
+                      <b>${types}</b>
+                  </div>
+                  <img id="pokemonNameCards" src="${pokemonImageSrc}">
+              </div>
           </div>
-        </div>
       `;
-    }
   }
+}
+
+
+
+
   
   
+
+
+
+
+
+
+
+
+
+
+
+
+// function renderAbilities(i, currentPokemon) {
+//   let stats = currentPokemon['stats'];
+//   let abilities = document.getElementById('abilities');
+  
+
+//   for (let i = 0; i < stats.length; i++) {
+//       let statsName = currentPokemon['stats'][i]['stat']['name'];
+//       let baseStat = currentPokemon['stats'][i]['base_stat'];
+
+//       if(statsName === 'hp') {
+//           statsName = 'HP';
+//       }
+
+//       if(statsName === 'special-attack') {
+//           statsName = 'Sp. Attack';
+//       }
+
+//       if(statsName === 'special-defense') {
+//           statsName = 'Sp. Defense';
+//       }
+
+
+// let pokemon = [
+//   'bulbasaur', 'ivysaur', 'venusaur', 'squirtle', 'charmander', 'pikachu', 'jigglypuff', 'meowth', 'psyduck', 'machop', 'geodude', 'gyarados', 'lapras', 'eevee', 'snorlax', 'articuno', 'zapdos', 'moltres', 'chikorita', 'cyndaquil', 'totodile', 'togepi'];
+  
+//   // 'mareep', 'typhlosion', 'feraligatr', 'lugia', 'ho-oh', 'treecko', 'torchic', 'mudkip', 'gardevoir', 'sceptile', 'blaziken', 'swampert', 'mightyena', 'zigzagoon', 'linoone', 'wurmple', 'silcoon', 'beautifly', 'cascoon'];
+  
+//   // 'dustox', 'lotad', 'lombre', 'ludicolo', 'seedot', 'nuzleaf', 'shiftry', 'taillow', 'swellow', 'wingull', 'pelipper', 'ralts', 'kirlia', 'gardevoir', 'surskit', 'masquerain', 'shroomish', 'breloom', 'slakoth', 'vigoroth', 'slaking', 'nincada', 'ninjask', 'shedinja', 'whismur', 'loudred', 'exploud', 'makuhita', 'hariyama', 'azurill', 'nosepass', 'skitty', 'delcatty', 'sableye', 'mawile', 'aron', 'lairon', 'aggron', 'meditite', 'medicham', 'electrike', 'manectric', 'plusle', 'minun', 'volbeat', 'illumise', 'roselia', 'gulpin', 'swalot', 'carvanha', 'sharpedo', 'wailmer', 'wailord', 'numel', 'camerupt', 'torkoal',];
